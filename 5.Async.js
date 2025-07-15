b9b1efc5aa9c357b8e2e02d471e54aca9dab7f9b
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-15 16:46:42 +0300

    Def: Asynchronous Javascript

---
+++ 5.Async.js
@@ -0,0 +1,12 @@
+// Asynchronous JavaScript allows code to run without blocking the main thread 
+// — meaning the rest of the program can continue running while waiting for tasks like:
+//      1.API calls
+//      2.Timers (setTimeout, setInterval)
+//      3.File reading
+//      4.Database queries
+// async behavior prevents blocking operations that could freeze your UI or app.
+
+// Main Tools For Async in Javascript:
+// a.Callbacks
+// b.Promises
+// c.Async/Await
