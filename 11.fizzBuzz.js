45b726c586d321d5764fa75b885f270d5cb8c0cb
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-16 12:47:40 +0300

    FizzBuzz

---
+++ 11.fizzBuzz.js
@@ -0,0 +1,11 @@
+for (let i=0; i<=100; ++i){
+    if(i % 2 === 0){
+        console.log(`${i}.Fizz`)
+    } else if(i % 5 === 0){
+        console.log(`${i}.Buzz`);
+    }
+    else{
+        console.log(`${i}.FizzBuzz`);
+    }
+    
+}
