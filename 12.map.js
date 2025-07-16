32881dd44b6384980413a592d90dcf5b3d497393
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-16 12:49:13 +0300

    Map

---
+++ 12.map.js
@@ -0,0 +1,20 @@
+//map
+
+     /* MAP METHOD */
+const stringArray = ['John', 'Kate', 'Kush', 'Jane']
+
+const capitalStrings = stringArray.map((name) => {
+    return name.toUpperCase();
+})
+console.log(capitalStrings);
+
+
+
+
+// const myMap = new Map();
+//     myMap.set('name', 'Alice');
+//     myMap.set(1, 'One');
+//     myMap.set(true, 'Boolean Value');
+
+//     console.log(myMap.get('name')); // Output: Alice
+//     console.log(myMap.size);      // Output: 3
