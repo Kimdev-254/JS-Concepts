7e931e7e0a4708b8493e5d2a92ffb009ddec59e5
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-17 21:14:37 +0300

    Destructuring

---
+++ 15.Destructuring.js
@@ -0,0 +1,7 @@
+// Destructuring assignment is a JavaScript expression that makes it possible to unpack values from arrays 
+// or properties from objects into distinct variables.
+
+const array = [1, 2, 3];
+const [a, b, c] = array;
+
+console.log(a, b, c); // 1 2 3
