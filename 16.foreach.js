b9b850558997fc86439295f9cb4b06c3aca7f319
Kimdev254 <bonifacekimani715@gmail.com>
2025-07-20 18:51:45 +0300

    foreach method

---
+++ 16.foreach.js
@@ -0,0 +1,7 @@
+// foreach method for arrays
+
+const numbers = [1, 2, 3, 4, 5];
+// Using forEach to iterate over the array
+numbers.forEach((number) => {
+  console.log(number * 2); // Output each number multiplied by 2
+}); // 2, 4, 6, 8, 10
