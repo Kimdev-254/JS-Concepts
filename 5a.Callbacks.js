9b5b1ca6b438d9b9051315023c39e8564936ac23
Kimdev254 <bonifacekimani715@gmail.com>
2025-08-13 22:45:12 +0300

    callback greet

--- 5a.Callbacks.js
+++ 5a.Callbacks.js
@@ -1,9 +1,7 @@
 // A callback is a function passed into another function
 
 //example
-function fetchData(callback) {
-    setTimeout(() => {
-        const data = { id: 1, name: 'John Doe' };
-        callback(data);
-    }, 1000);
-}
+function greet(name, callback) {
+    console.log("Hello " + name);
+    callback();
+}   
